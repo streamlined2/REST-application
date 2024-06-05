08a333f4810b534a4738ef7705d29d44ed12f00d
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2024-06-05 15:40:03 +0300

    Fixed Kafka message serializer; it does not use class file to save data

--- src/main/java/com/streamlined/restapp/service/notification/MessageSerializer.java
+++ src/main/java/com/streamlined/restapp/service/notification/MessageSerializer.java
@@ -1,8 +1,8 @@
 package com.streamlined.restapp.service.notification;
 
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import org.apache.kafka.common.serialization.Serializer;
 import com.streamlined.restapp.exception.KafkaMessageSerializationException;
 
@@ -14,15 +14,24 @@ public class MessageSerializer implements Serializer<Message> {
 	private static final int INITIAL_BYTE_STREAM_SIZE = 1000;
 
 	@Override
-	public byte[] serialize(String topic, Message event) {
+	public byte[] serialize(String topic, Message message) {
 		try (ByteArrayOutputStream byteStream = new ByteArrayOutputStream(INITIAL_BYTE_STREAM_SIZE);
-				ObjectOutputStream dataStream = new ObjectOutputStream(byteStream)) {
-			dataStream.writeObject(event);
+				DataOutputStream dataStream = new DataOutputStream(byteStream)) {
+			dataStream.writeLong(message.getInstant().getEpochSecond());
+			dataStream.writeUTF(message.getSender().getName());
+			dataStream.writeUTF(message.getSender().getEmail());
+			dataStream.writeInt(message.getRecipients().size());
+			for (Contact recipient : message.getRecipients()) {
+				dataStream.writeUTF(recipient.getName());
+				dataStream.writeUTF(recipient.getEmail());
+			}
+			dataStream.writeUTF(message.getSubject());
+			dataStream.writeUTF(message.getContent());
 			return byteStream.toByteArray();
 		} catch (IOException e) {
-			log.error("impossible to serialize Kafka message {}", event.toString());
+			log.error("impossible to serialize Kafka message {}", message.toString());
 			throw new KafkaMessageSerializationException(
-					"impossible to serialize Kafka message %s".formatted(event.toString()), e);
+					"impossible to serialize Kafka message %s".formatted(message.toString()), e);
 		}
 	}
 
